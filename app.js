44b334c1737ad38036b8ba9e372997a56388e31a
Nehal Ali <nehalalikamran103@gmail.com>
2025-10-29 13:47:08 -0700

    Assignment 3 done

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var meraName = "I am 18 year old";
+
+alert (meraName);
+
+var text = " You have this site 30 times";
+
+alert (text);
+
+var dateYear = "My Birth Year 2007\nDate Type of my declared Variable is Number";
+
+alert (dateYear);
+
+var xyz = "John Doe orderd 5 t-shirt(s) on XYZ clothing store";
+
+alert (xyz);
